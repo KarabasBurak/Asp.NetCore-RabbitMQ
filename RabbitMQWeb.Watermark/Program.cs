659699831405e92d35a72ba680fcd7c863e267c7
KarabasBurak <117377125+KarabasBurak@users.noreply.github.com>
2024-05-27 09:39:12 +0300

    ImageWatermarkProcessBackgroundService added and finish

--- RabbitMQWeb.Watermark/Program.cs
+++ RabbitMQWeb.Watermark/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
+using RabbitMQWeb.Watermark.BackgroundServices;
 using RabbitMQWeb.Watermark.Models;
 using RabbitMQWeb.Watermark.Services;
 using System.Configuration;
@@ -12,9 +13,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase(databaseName: "productDb");
 });
 
-builder.Services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQ")) });
+builder.Services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQ")),DispatchConsumersAsync=true });
 builder.Services.AddSingleton<RabbitMQClientService>();
 builder.Services.AddSingleton<RabbitMQPublisher>();
+builder.Services.AddHostedService<ImageWatermarkProcessBackgroundService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
